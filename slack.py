06c1fad859be888cb8a0abc60690bc10f353353a
Stephen Adams <tsadams@gmail.com>
2019-01-25 12:58:43 -0500

    Add slack message capability

---
+++ slack.py
@@ -0,0 +1,10 @@
+import os
+import requests
+
+SLACK_TOKEN = os.getenv('SLACK_TOKEN', 'T026NJJ6Z/BFQF13KHD/qSohzFHa6qt8GeUah8TLo0dY')
+SLACK_URL = "https://hooks.slack.com/services/"
+
+def send_message(msg):
+
+    message_json = {"text": msg}
+    requests.get(SLACK_URL + SLACK_TOKEN, json=message_json)
